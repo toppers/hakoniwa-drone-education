b0344c8056b32a48552f33ad8da5e24ba4c889b7
Takashi Mori <kanetugu2015@gmail.com>
2024-10-18 06:59:36 +0900

    bug fixed

--- src/drone_evaluation/evaluate-phase-gains.bash
+++ src/drone_evaluation/evaluate-phase-gains.bash
@@ -6,7 +6,7 @@ if [ $# -ne 1 ]; then
 fi
 
 INPUT_DIR="$1"
-DIRNAME=$(basename ${INPUT_DIR})
+DIRNAME=$(basename "${INPUT_DIR}")
 
 LINE_NUM=$(($(wc -l < "${INPUT_DIR}/test_pattern.csv") - 1))
 
@@ -17,7 +17,7 @@ fi
 
 mkdir -p test-results/${DIRNAME}
 rm -rf test-results/${DIRNAME}/*
-echo "freq, log_freq, gain, phase, phase1_at_freq, phase2_at_freq" > test-results//${DIRNAME}/result.csv
+echo "freq, log_freq, gain, phase, phase1_at_freq, phase2_at_freq" > test-results/${DIRNAME}/result.csv
 for i in $(seq "${LINE_NUM}"); do
     index=$((i - 1))
     echo "index: ${index}"
@@ -26,10 +26,11 @@ for i in $(seq "${LINE_NUM}"); do
             ../src/drone_evaluation/template/sine-input.json ${index}
     bash -x ../src/drone_evaluation/evaluate.bash sine-input-updated.json
     python ../src/drone_evaluation/freq_evaluator.py ./sine-input-updated.json \
-        >> test-results//${DIRNAME}/result.csv
-    FREQ="freq_$( tail -n ${i} ../src/drone_evaluation/input/controller-spd_z/test_pattern.csv | awk -F, '{print $1}')"
-    mkdir -p test-results//${DIRNAME}/${FREQ}
+        >> test-results/${DIRNAME}/result.csv
+    sed_line_no=$(expr $i + 1)
+    FREQ="freq_$(sed -n "${sed_line_no}p" "${INPUT_DIR}/test_pattern.csv" | awk -F, '{print $1}')"
+    mkdir -p test-results/${DIRNAME}/${FREQ}
     mv sine-input-updated.json test-results/${DIRNAME}/${FREQ}/
     mv in.csv test-results/${DIRNAME}/${FREQ}/
-    mv drone_log0 test-results//${DIRNAME}/${FREQ}/
+    mv drone_log0 test-results/${DIRNAME}/${FREQ}/
 done
