987acc8dc8000f15264d4e86f6bed9a384320d7f
Takashi Mori <kanetugu2015@gmail.com>
2024-10-22 05:48:27 +0900

    add step response

--- src/libs/pid_param_view.py
+++ src/libs/pid_param_view.py
@@ -11,29 +11,34 @@ import numpy as np
 matplotlib.use('Qt5Agg')
 
 global tfd
-
 class PIDSliderApp(QWidget):
     def __init__(self):
-        super().__init__()
-        self.initUI()
         self.saved_x_limit = 1
         self.saved_y_limit = 1
+        self.scale_slider = 1000
+        self.scale_param = 10
+        super().__init__()
+        self.initUI()
 
         # matplotlibのFigureとAxisを初期化
         self.figure, self.ax = plt.subplots()
         plt.ion()  # インタラクティブモードを有効化
         plt.show()  # 最初に一度だけFigureを表示
 
+        # ステップ応答のためのFigureとAxisを初期化
+        self.figure_step, self.ax_step = plt.subplots()
+        plt.ion()  # インタラクティブモードを有効化
+        plt.show()  # 最初に一度だけFigureを表示
+
         # TransParserから初期値を取得
-        self.kp_init = tfd.constants.get("VAlt_Kp", 0) * 10  # 10倍してスライダー用に変換
-        self.ki_init = tfd.constants.get("VAlt_Ki", 0) * 10
-        self.kd_init = tfd.constants.get("VAlt_Kd", 0) * 10
+        self.kp_init = tfd.constants.get("VAlt_Kp", 0) * self.scale_param
+        self.ki_init = tfd.constants.get("VAlt_Ki", 0) * self.scale_param
+        self.kd_init = tfd.constants.get("VAlt_Kd", 0) * self.scale_param
 
         # 初期値をスライダーに反映
         self.slider_p.setValue(int(self.kp_init))
         self.slider_i.setValue(int(self.ki_init))
         self.slider_d.setValue(int(self.kd_init))
-
 
     def initUI(self):
         layout = QVBoxLayout()
@@ -43,7 +48,7 @@ class PIDSliderApp(QWidget):
         layout.addWidget(self.label_p)
         self.slider_p = QSlider(Qt.Horizontal, self)
         self.slider_p.setMinimum(0)
-        self.slider_p.setMaximum(100)
+        self.slider_p.setMaximum(self.scale_slider)
         self.slider_p.valueChanged.connect(self.update_p)
         layout.addWidget(self.slider_p)
 
@@ -51,7 +56,7 @@ class PIDSliderApp(QWidget):
         layout.addWidget(self.label_i)
         self.slider_i = QSlider(Qt.Horizontal, self)
         self.slider_i.setMinimum(0)
-        self.slider_i.setMaximum(100)
+        self.slider_i.setMaximum(self.scale_slider)
         self.slider_i.valueChanged.connect(self.update_i)
         layout.addWidget(self.slider_i)
 
@@ -59,7 +64,7 @@ class PIDSliderApp(QWidget):
         layout.addWidget(self.label_d)
         self.slider_d = QSlider(Qt.Horizontal, self)
         self.slider_d.setMinimum(0)
-        self.slider_d.setMaximum(100)
+        self.slider_d.setMaximum(self.scale_slider)
         self.slider_d.valueChanged.connect(self.update_d)
         layout.addWidget(self.slider_d)
 
@@ -67,35 +72,60 @@ class PIDSliderApp(QWidget):
         self.setWindowTitle('PID パラメータ調整')
         self.show()
 
+    def update_graph(self):
+        # コントローラとプラントの伝達関数を計算
+        self.W_num, self.W_den = tfd.calculate_w()
+
+        self.update_poles()
+        self.update_step_response()
+
     def update_poles(self):
         # コントローラとプラントの伝達関数を計算
-        W_num, W_den = tfd.calculate_w()
-        num = tfd.get_coefficients(W_num)
-        den = tfd.get_coefficients(W_den)
+        num = tfd.get_coefficients(self.W_num)
+        den = tfd.get_coefficients(self.W_den)
 
         # 極をプロット（Figureを再利用）
         self.ax.clear()  # 以前のプロットをクリア
         self.plot_poles(num, den)  # 極を再描画
         plt.draw()  # 描画を更新
 
+
+    def update_step_response(self):
+        # コントローラとプラントの伝達関数を計算
+        num = tfd.get_coefficients(self.W_num)
+        den = tfd.get_coefficients(self.W_den)
+        system = ctrl.TransferFunction(num, den)
+
+        # ステップ応答の計算
+        t, y = ctrl.step_response(system)
+
+        # ステップ応答のプロットをクリアして更新
+        self.ax_step.clear()
+        self.ax_step.plot(t, y, label='Step Response')
+        self.ax_step.set_title('Step Response')
+        self.ax_step.set_xlabel('Time (s)')
+        self.ax_step.set_ylabel('Amplitude')
+        self.ax_step.grid(True)
+        self.ax_step.legend()
+        plt.draw()  # 描画を更新
+
     def update_p(self, value):
         # Pパラメータの値を更新
         tfd.update_constant("VAlt_Kp", value / 10.0)  # 値を変換して更新
-        
-        self.update_poles()
+        self.update_graph()
         # Pの値をラベルに反映
         self.label_p.setText(f"P: {value}")
 
     def update_i(self, value):
         # Iパラメータの値を更新
         tfd.update_constant("VAlt_Ki", value / 10.0)
-        self.update_poles()
+        self.update_graph()
         self.label_i.setText(f"I: {value}")
 
     def update_d(self, value):
         # Dパラメータの値を更新
         tfd.update_constant("VAlt_Kd", value / 10.0)
-        self.update_poles()
+        self.update_graph()
         self.label_d.setText(f"D: {value}")
 
     def plot_poles(self, num, den):
@@ -119,8 +149,6 @@ class PIDSliderApp(QWidget):
         margin = 1.1
         x_limit = margin * max_real
         y_limit = margin * max_imag
-        #self.saved_x_limit = max(self.saved_x_limit, x_limit)
-        #self.saved_y_limit = max(self.saved_y_limit, y_limit)
         self.saved_x_limit = x_limit
         self.saved_y_limit = y_limit
 
